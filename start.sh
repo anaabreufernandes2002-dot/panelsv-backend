15f250d5b9cd6376a1db40e1efa8c58b50b069b8
Ana Abreu <ana.abreu.fernandes2002@gmail.com>
2026-03-27 15:50:52 -0400

    fix: railway usando sqlite local do container

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 set -e
-
-mkdir -p /app/seed/uploads
-
-DB_PATH="/app/seed/panelsv"
-
-exec java -Dspring.datasource.url=jdbc:sqlite:$DB_PATH -jar $(find /app/target -name "*.jar" | head -n 1)
+exec java -jar $(find /app/target -name "*.jar" | head -n 1)
